46d296047043f8b1f91cda1d21bce5697625bd59
Roee Malis <roee.malis@vonage.com>
2020-10-20 13:03:36 +0300

    fix: Correctly detect MFC as webcomponent when passing an instance in the constructor options (#7)

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,7 @@ module.exports = {
   },
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70
+      lines: 70
     }
   },
   collectCoverage: true,
